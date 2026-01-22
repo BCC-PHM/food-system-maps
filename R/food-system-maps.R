28d5be6d75587d894c7d636e6e0805e63bb0bb82
David.Ellis <david.ellis@birmingham.gov.uk>
2026-01-22 13:21:21 +0000

    Plotted interactive and basic static maps with colouring

--- R/food-system-maps.R
+++ R/food-system-maps.R
@@ -11,11 +11,17 @@ colours = list(
   "Food Waste & Recycling" = "#008547"
 )
 
+credits <- paste0("Contains OS data \u00A9 Crown copyright and database right ",
+                 # Get current year
+                 format(Sys.Date(), "%Y"),
+                 ". Source:\nOffice for National Statistics licensed under the Open Government Licence v.3.0."
+)
+
 data <- read_excel("data/BFLF Postcodes.xlsx")
 
 main <- data %>%
   select(
-    "Recipient Org:Name", "Main location", "Main location postcode"
+    "Recipient Org:Name", "Main location", "Main location postcode", "Primary Workstream"
   ) %>%
   rename(
     Name = `Recipient Org:Name`, 
@@ -28,7 +34,7 @@ main <- data %>%
 
 other1 <- data %>%
   select(
-    "Recipient Org:Name", "Other location 1", "Other location 1 postcode"
+    "Recipient Org:Name", "Other location 1", "Other location 1 postcode", "Primary Workstream"
   ) %>%
   rename(
     Name = `Recipient Org:Name`,
@@ -44,7 +50,7 @@ other1 <- data %>%
 
 other2 <- data %>%
   select(
-    "Recipient Org:Name", "Other location 2", "Other location 2 postcode"
+    "Recipient Org:Name", "Other location 2", "Other location 2 postcode", "Primary Workstream"
   ) %>%
   rename(
     Name = `Recipient Org:Name`,
@@ -66,7 +72,7 @@ data_long <- get_long_lat(
   )
 ) %>%
   select(
-    c("Name", "Address", "Postcode", "Type", "LAT", "LONG")
+    c("Name", "Address", "Postcode", "Type", "Primary Workstream", "LAT", "LONG")
   )
 
 points_shape <- sf::st_as_sf(
@@ -78,11 +84,83 @@ points_shape <- sf::st_as_sf(
     -c(LONG, LAT)
   )
 
+# Fix column names
+colnames(points_shape) <- gsub("\\.", " ", colnames(points_shape))
+
+Brum_Wards <- sf::st_as_sf(Ward) %>%
+  filter(
+    Area == "Birmingham"
+  )
+
+################################################################################
+#                          Plot interactive map                                # 
+################################################################################
 
 map <- tm_shape(
-  Ward
+  Brum_Wards
   ) +
   tm_borders() +
   tm_shape(points_shape) +
-  tm_dots()
-map
+  tm_dots(
+    fill = "Primary Workstream",
+    size = 0.5,
+    fill.scale = tm_scale(
+      breaks = names(colours),
+      values = colours
+    ),
+    col = "#1A1A1A",
+    shape = 21,
+    lwd = 1.5
+  ) +
+  tm_credits(
+    credits,
+    size = 0.6,
+    position = c("LEFT", "BOTTOM")
+  )
+
+save_map(map, "output/food-systems-interactive.html")
+
+
+################################################################################
+#                        Plot report map - no labels                           # 
+################################################################################
+
+map2 <- tm_shape(
+  Brum_Wards
+) +
+  tm_borders() +
+  tm_shape(points_shape) +
+  tm_dots(
+    fill = "Primary Workstream",
+    size = 0.5,
+    fill.scale = tm_scale(
+      breaks = names(colours),
+      values = colours
+    ),
+    col = "#1A1A1A",
+    shape = 21,
+    lwd = 1.5
+  ) +
+  tm_credits(
+    credits,
+    size = 0.6,
+    position = c("LEFT", "BOTTOM")
+  ) +
+  tm_layout(
+    legend.position = c("LEFT", "TOP"),
+    scale = 0.8,
+    legend.height = 8,
+    legend.frame.alpha = 0,
+    legend.frame.lwd = 0,
+    legend.bg.alpha = 0.4,
+    inner.margins = 0.08,
+    frame = FALSE) +
+  tm_compass(
+    type = "8star",
+    size = 4,
+    position = c("right", "bottom"),
+    color.light = "white"
+  )
+
+save_map(map2, "output/food-systems.png")
+
